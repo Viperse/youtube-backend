2ba6725e3bd27e2f418b387aca5418ceb5e4e197
Viperse <129846485+Viperse@users.noreply.github.com>
2023-09-06 17:46:54 +0900

    feat: index,mustache 생성, controller 추가 vo(domain) 8개 추가

---
+++ src/main/java/com/kh/youtube/domain/Channel.java
@@ -0,0 +1,20 @@
+package com.kh.youtube.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Channel {
+
+    private int channelCode;
+    private String channelName;
+    private Date channelDate;
+
+    private Member member;
+}
