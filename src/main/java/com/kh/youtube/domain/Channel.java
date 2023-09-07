d0ea2856b30b60932373b34cf551109394f2f5c8
Viperse <129846485+Viperse@users.noreply.github.com>
2023-09-07 17:05:41 +0900

    feat: youtube restcontroller Category crud 기능

--- src/main/java/com/kh/youtube/domain/Channel.java
+++ src/main/java/com/kh/youtube/domain/Channel.java
@@ -1,6 +1,8 @@
 package com.kh.youtube.domain;
 
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,8 @@ public class Channel {
     private int channelCode;
     private String channelName;
     private Date channelDate;
-
+    
+    @ManyToOne // Channel 엔티티와 Member 엔티티를 다대일 관계로 설정
+    @JoinColumn(name = "member_id") // 외래키 생성 or Member 엔티티의 기본키와 매핑
     private Member member;
 }
